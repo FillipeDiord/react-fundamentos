dfd10561cca74007e49a65438cd13acc2f0e28ba
Fillipe Diord <fillipe.diord@hotmail.com>
2022-07-31 18:53:50 -0300

    dynamic classes

--- src/components/App.js
+++ src/components/App.js
@@ -10,10 +10,10 @@ import styles from '../App.scss';
 export function App() {
 
   const [posts, setPosts] = useState([
-    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false },
-    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: false },
-    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 30, read: true },
-    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 60, read: false },
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: true, removed: true },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: false, removed: false },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 30, read: true, removed: false },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 60, read: false, removed: false },
   ]);
 
   function handleRefresh() {
@@ -31,8 +31,12 @@ export function App() {
   }
 
   function handleRemovePost(postId) {
-    setPosts((prevState) => (
-      prevState.filter(post => post.id !== postId)
+    setPosts((prevState) => prevState.map(
+      post => (
+        post.id === postId
+          ? { ...post, removed: true }
+          : post
+      )
     ));
   }
 
