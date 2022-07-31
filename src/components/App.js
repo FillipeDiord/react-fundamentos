f0e58c1068ea1a8fad0d15ede8beb84738367ebe
Fillipe Diord <fillipe.diord@hotmail.com>
2022-07-31 18:06:04 -0300

    changing file extensions to scss

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { Post } from './Post';
 import { Header } from './Header';
 import { ThemeProvider } from '../ThemeContext';
 
-import styles from '../App.css';
+import styles from '../App.scss';
 
 
 export function App() {
