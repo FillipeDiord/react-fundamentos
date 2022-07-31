09e5849dfa9c4c833eb52e41df82dc9a7afdd305
Fillipe Diord <fillipe.diord@hotmail.com>
2022-07-31 10:57:40 -0300

    Organizing Theme Provider code

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,18 @@
-import React, { useState, createContext } from 'react';
+import React, { useState } from 'react';
 
 import { Post } from './Post';
 import { Header } from './Header';
-
-
-export const ThemeContext = createContext('dark');
+import { ThemeProvider } from '../ThemeContext';
 
 
 export function App() {
-  const [theme, setTheme] = useState('dark');
-
+  
   const [posts, setPosts] = useState([
     { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false },
     { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: false },
     { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 30, read: true },
     { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 60, read: false },
   ]);
-
-  function handleToggleTheme() {
-    setTheme((prevState) => (
-      prevState === 'dark' ? 'ligth' : 'dark'));
-  }
 
   function handleRefresh() {
 
@@ -43,12 +35,7 @@ export function App() {
   }
 
   return (
-    <ThemeContext.Provider
-      value={{
-        theme,
-        onToggleTheme: handleToggleTheme,
-      }}
-    >
+    <ThemeProvider>
       <Header>
         <h2>Posts da semana
           <button onClick={handleRefresh}>Atualizar</button>
@@ -64,6 +51,6 @@ export function App() {
           post={post}
         />
       ))}
-    </ThemeContext.Provider>
+    </ThemeProvider>
   );
 }
