363d6e6e01acb4a40b102df1bd44070cf0190574
Fillipe Diord <fillipe.diord@hotmail.com>
2022-08-01 08:20:57 -0300

    create global styles

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 
 import { App } from './components/App';
 
+import GlobalStyle from './styles/global';
 
 ReactDOM.render(
-  <App />,
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
   document.getElementById('root'),
 );
